c25316014abefda5d13825f3b9a6b877481d8ce0
Patryk Diak <pdiak@wi.zut.edu.pl>
2012-05-15 00:03:04 +0200

    Dodanie klas  HighScores i Score oraz ich wlasciwosci

---
+++ src/Game/HighScores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    class HighScores
+    {
+        private List<Score> scores;
+        private const int maxCapacity = 10;
+        public HighScores()
+        {
+            scores = new List<Score>(maxCapacity);
+        }
+    }
+}
