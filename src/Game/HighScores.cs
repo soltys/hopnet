d1c49fd0e2258c960cb8212e15f4cb7667f11acd
Patryk Diak <pdiak@wi.zut.edu.pl>
2012-05-15 00:06:32 +0200

    Dodanie metod Add() oraz Clear() do klasy HighScores

--- src/Game/HighScores.cs
+++ src/Game/HighScores.cs
@@ -13,5 +13,41 @@ namespace Game
         {
             scores = new List<Score>(maxCapacity);
         }
+        public void Add(Score score)
+        {
+            try
+            {
+                if (score.Points < 0)
+                {
+                    throw new ArgumentException();
+                }
+                if (scores.Count == scores.Capacity)
+                {
+                    if (scores.Min().Points < score.Points)
+                    {
+                        scores.Remove(scores.Min());
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                scores.Add(score);
+            }
+            catch (NullReferenceException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+        }
+        public void Clear()
+        {
+            scores.Clear();
+        }
     }
 }
