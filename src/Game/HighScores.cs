9a1531480990bee2622236eedd86e8d499eabb37
Patryk Diak <pdiak@wi.zut.edu.pl>
2012-05-15 20:39:52 +0200

    Dodanie metod Load() Save() SortDescending() do klasy HighScores

--- src/Game/HighScores.cs
+++ src/Game/HighScores.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Collections;
-//using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Security;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Game
 {
@@ -23,7 +26,7 @@ namespace Game
                 {
                     throw new ArgumentException();
                 }
-                if (scores.Count == scores.Capacity)
+                if (scores.Count == maxCapacity)
                 {
                     if (scores.Min().Points < score.Points)
                     {
@@ -51,7 +54,84 @@ namespace Game
         {
             scores.Clear();
         }
+        public void Load()
+        {
+            try
+            {
+                IsolatedStorageFile isoStorage = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+                if (isoStorage.FileExists("highscores.xml"))
+                {
+                    XmlSerializer xSerializer = new XmlSerializer(typeof(List<Score>));
+                    using (IsolatedStorageFileStream oStream = new IsolatedStorageFileStream("highscores.xml", FileMode.Open, isoStorage))
+                    {
 
+                        using (XmlReader xReader = XmlReader.Create(oStream))
+                        {
+                            List<Score> tmpScores = new List<Score>((List<Score>)xSerializer.Deserialize(xReader));
+                            if (tmpScores.Count > maxCapacity)
+                            {
+                                throw new InvalidDataException();
+                            }
+                            else
+                            {
+                                scores = new List<Score>(tmpScores);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IsolatedStorageException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+            catch (IOException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+            catch (SecurityException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+        }
+        public void Save()
+        {
+            try
+            {
+                IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+                XmlSerializer xSerializer = new XmlSerializer(typeof(List<Score>));
+
+                using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream("highscores.xml", FileMode.Create, isoStore))
+                {
+
+                    using (StreamWriter sWriter = new StreamWriter(isoStream))
+                    {
+                        xSerializer.Serialize(sWriter, scores);
+                    }
+                }
+            }
+            catch (IsolatedStorageException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+            catch (IOException)
+            {
+                //TODO: Handlig it
+                throw;
+            }
+        }
+        public void SortDescending()
+        {
+            scores = scores.OrderByDescending(s => s.Points).ThenBy(s => s.Name).ToList<Score>();
+        }
         public IEnumerator<Score> GetEnumerator()
         {
             return scores.GetEnumerator();
