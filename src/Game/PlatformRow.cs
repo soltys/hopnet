90eff2ade9aae10aaa26bf2603524b2b072d5b9b
Jerzy Goworski <jgoworski@wi.ps.pl>
2012-05-25 20:51:07 +0200

    nic znaczacego

--- src/Game/PlatformRow.cs
+++ src/Game/PlatformRow.cs
@@ -44,18 +44,7 @@ namespace Game
             return true;
         }
 
-        public bool IsFull()
-        {
-            int counter = 0;
-            for (int i = 0; i < RowLength; i++)
-            {
-                if (platformSettings[i] == true)
-                {
-                    counter++;
-                }
-            }
-            if (counter == PlatformRow.RowLength) { return true; }
-            else { return false; }
-        }
+
+
     }
 }
